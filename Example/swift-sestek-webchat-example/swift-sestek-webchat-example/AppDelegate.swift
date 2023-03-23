62c7d73054a8d853849fdaea68b6c9a3c04170bb
Tayfun Akgün <tayfun.akgun@sestek.com>
2023-03-23 16:17:48 +0300

    Release 0.0.6

--- Example/swift-sestek-webchat-example/swift-sestek-webchat-example/AppDelegate.swift
+++ Example/swift-sestek-webchat-example/swift-sestek-webchat-example/AppDelegate.swift
@@ -14,8 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let defaultConfiguration = DefaultConfiguration(clientId: "mobile-testing", tenant: "Tayfun", channel: "NdaInfoBip", project: "GocIdaresi_TR", fullName: "Ömer Sezer",customActionData: "tayfuntest")
+        let defaultConfiguration = DefaultConfiguration(clientId: "mobile-testing", tenant: "Tayfun", channel: "NdaInfoBip", project: "GocIdaresi_TR", fullName: "Ömer Sezer",customActionData: "testing_customdata")
         SestekWebChat.shared.initLibrary(defaultConfiguration: defaultConfiguration)
         return true
     }
 }
+
